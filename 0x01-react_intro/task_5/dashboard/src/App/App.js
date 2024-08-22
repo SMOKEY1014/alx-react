ea386986ec39e208b7c8e48bcdac1cc5a5d3ff09
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-08-22 15:21:19 +0200

    tasks

--- 0x01-react_intro/task_5/dashboard/src/App/App.js
+++ 0x01-react_intro/task_5/dashboard/src/App/App.js
@@ -1,6 +1,7 @@
-import logo from "./holberton-logo.jpg";
+import React from "react";
+import logo from "../assets/holberton-logo.jpg";
 import "./App.css";
-import { getFullYear, getFooterCopy } from "./utils";
+import { getFullYear, getFooterCopy } from "../utils/utils";
 
 function App() {
   return (
@@ -11,6 +12,13 @@ function App() {
       </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
+        <form>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email"></input>
+          <label htmlFor="password">Password:</label>
+          <input type="password" name="password"></input>
+          <button>OK</button>
+        </form>
       </div>
       <div className="App-footer">
         Copyright {getFullYear()} - {getFooterCopy()}
